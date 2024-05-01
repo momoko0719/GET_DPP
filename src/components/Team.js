4b31fbb93efb4a15ea5c5ffd408cc48cca307e0e
wendy479 <wendy479@uw.edu>
2024-04-30 23:44:56 -0700

    abt page updates

--- src/components/Team.js
+++ src/components/Team.js
@@ -30,9 +30,7 @@ const Team = () => {
             alt="Frank Martinez"
             className="card-img"
           />
-          <div className="card-name">Frank Martinez
-          <br/>
-          (Our Sponsor)
+          <div className="card-name">Frank Martinez (Sponsor)
           </div>
         </div>
         <div className="aboutcard">
@@ -54,6 +52,79 @@ const Team = () => {
         <div className="aboutcard">
           <img src="/imgs/wendy.jpg" alt="Wendy Huang" className="card-img" />
           <div className="card-name">Wendy Huang</div>
+        </div>
+      </div>
+      <div className="section2" id="section2">
+        <div class="section2-top">
+          <div class="section2-title">
+            Discovering Our Drive:
+            <br />
+            Understanding Type 2 Diabetes in the Latino Community
+            <br />
+          </div>
+          <div class="section2-quote">
+            “Maybe add a quotation from Dr.morales here — Dr. Morales”
+          </div>
+        </div>
+        <div className="section2-bottom">
+          <div class="card mb-5">
+            <div class="card-header">
+              <p>
+                Why Is Type 2 Diabetes Our Focus?
+              </p>
+              <img class="cardicon" src="imgs/faqIcon.png" alt="faq icon img" />
+            </div>
+            <div class="card-body">
+              <p class="card-text">
+              Did you know that 1 in 10 Americans live with type 2 diabetes? It’s not just a statistic;
+              it’s about real people facing real challenges. That’s where Dr. Dos comes in. We focus on
+              type 2 diabetes because it’s largely preventable with the right lifestyle changes.
+              We believe everyone should have the accessible tools to manage their health, and we’re here to make
+              that easier with practical tips and personalized advice.
+              </p>
+            </div>
+          </div>
+
+          <div class="card mb-5">
+            <div class="card-header">
+              <p>
+                Why Do We Prioritize the Latino Community?
+              </p>
+              <img
+                class="cardicon"
+                src="imgs/chartIcon.png"
+                alt="chart icon img"
+              />
+            </div>
+            <div class="card-body">
+              <p class="card-text">
+              Because minority community like Latino have higher risks of type 2 diabetes and often, fewer resources to fight it.
+              Our inspiration came from our sponsor, Frank Martinez, who saw firsthand how health crises like COVID-19 and diabetes
+              hit harder in the Easter Washington Latino community. Dr. Dos is our way of bridging that gap with a platform that’s not only free but also accessible
+              wherever you are, in both English and Spanish.
+              </p>
+            </div>
+          </div>
+
+          <div class="card mb-5">
+            <div class="card-header">
+              <p>
+                Why Are Diabetes Rates Higher in the Latino Community?
+              </p>
+              <img
+                class="cardicon"
+                src="imgs/fireIcon.png"
+                alt="fire icon img"
+              />
+            </div>
+            <div class="card-body">
+              <p class="card-text">
+                Diabetes rates are higher in the Latino community due to factos like language barriers, limited healthcare access,
+                and fewer healthy living resources. At Dr. Dos, we’re not just about recognizing these barriers; we’re about breaking them down.
+                Our app is designed to be a friendly guide in your pocket, helping you navigate diabetes prevention in ways that fit your life and culture.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
       <div className="abt-section3">
